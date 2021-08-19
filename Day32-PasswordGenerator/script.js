a804421f362d0edf932c88549b78f7f77a15ee32
Jackson Jacob <sergiojacksonramos@gmail.com>
2021-08-19 10:53:13 +0530

    added completed day32

--- Day32-PasswordGenerator/script.js
+++ Day32-PasswordGenerator/script.js
@@ -15,6 +15,20 @@ const randomFunc = {
     symbol: getRandomSymbols
 };
 
+clipboardEl.addEventListener('click', () => {
+    const textArea = document.createElement('textarea');
+    const password = resultEl.innerText;
+
+    if (!password) {return}
+
+    textArea.value = password;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    textArea.remove();
+    alert("Password copied to clipboard");
+});
+
 generateEl.addEventListener('click', () => {
     const length = +lengthEl.value;
     const hasLower = lowercaseEl.checked;
@@ -32,6 +46,20 @@ function generatePassword(lower, upper, number, symbol, length)
     const typesCount = lower+upper+number+symbol;
     const typesArray = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
     
+    if (typesCount === 0) {
+        return '';
+    }
+
+    for(let i = 0; i < length; i += typesCount) {
+        typesArray.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]()
+        })
+    }
+
+    const finalPassword = generatedPassword.slice(0, length);
+
+    return finalPassword;
 }
 
 function getRandomLower() {
